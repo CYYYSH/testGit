7836367cd76a3576ddee5d3a02e27823b1cc0031
ysh <1398425771@qq.com>
2022-04-10 23:02:42 +0800

    111

--- src/main/java/com/itysh/test/testgit.java
+++ src/main/java/com/itysh/test/testgit.java
@@ -3,5 +3,6 @@ package com.itysh.test;
 public class testgit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello world");
     }
 }
